5a83ba7a6f245be8af06eeb96ce0e6abe5714299
MoonC432 <rahulmunshi0@gmail.com>
2021-05-31 11:08:55 +0545

    Deploy backend

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = config('SECRET_KEY')
 DEBUG = False
 
 
-ALLOWED_HOSTS = ['127.0.0.1', ]
+ALLOWED_HOSTS = ['127.0.0.1', 'saicomputerserver.herokuapp.com/']
 
 
 # Application definition
